9c7ef1c354b6422212a6b0f754970def8efa31ae
Paul Popat <paul@popatux.co.uk>
2022-07-24 22:50:15 +0100

    Added spinners

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,7 +5,8 @@ import { ColourNames, GetColour } from "Src/Theme";
 import { IsOneOf } from "Src/utils/Type";
 // @ts-ignore: Importing CSS file content
 import style from "remixicon/fonts/remixicon.css";
-import Css, { Rule } from "Src/CSS";
+import Css, { Keyframes, Rule } from "Src/CSS";
+import Animation from "Src/styles/Animation";
 
 Define(
   "p-icon",
@@ -14,6 +15,7 @@ Define(
     size: IsString,
     colour: IsOneOf(...ColourNames),
     text: Optional(IsLiteral(true)),
+    spin: Optional(IsLiteral(true)),
   },
   {},
   {
@@ -22,12 +24,27 @@ Define(
     },
     css() {
       const colour = GetColour(this.Props.colour);
-      return Css.Init().With(
+      let result = Css.Init().With(
         Rule.Init("span")
           .With("line-height", "1.2")
           .With("font-size", this.Props.size)
           .With(this.Props.text ? colour.OnlyText() : colour.AsText())
       );
+
+      if (this.Props.spin)
+        result = result
+          .With(
+            Keyframes.Init("spin")
+              .With(Rule.Init("from").With("transform", "rotate(0deg)"))
+              .With(Rule.Init("to").With("transform", "rotate(360deg)"))
+          )
+          .With(
+            Rule.Init("span:before")
+              .With(new Animation("spin", "slow", "infinite"))
+              .With("display", "inline-block")
+          );
+
+      return result;
     },
     additional_css: style,
   }
