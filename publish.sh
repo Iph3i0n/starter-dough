0512625689dd094d5f66cf9a4c399d35c657114e
Paul Popat <paul@popatux.co.uk>
2022-08-02 14:01:03 +0100

    Fixed up pipeline

--- publish.sh
+++ publish.sh
@@ -2,5 +2,6 @@
 rm -r dist
 NODE_ENV='production' webpack
 cp ./package.json ./dist/package.json
-RELEASE_VERSION=${GITHUB_REF#refs/*/}
 sed -i 's/VERSION_NUMBER/$RELEASE_VERSION/g' ./dist/package.json
+cd ./dist
+npm publish --access public
